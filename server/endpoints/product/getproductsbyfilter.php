7e47cd2535b9724bd0616005848017d014fd0de0
Anchie <wyhwtf@gmail.com>
2025-02-09 13:44:51 +0100

    small changes

--- server/endpoints/product/getproductsbyfilter.php
+++ server/endpoints/product/getproductsbyfilter.php
@@ -4,6 +4,7 @@
 header("Access-Control-Allow-Origin: http://192.168.0.13:5173");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Credentials: true");
 
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
